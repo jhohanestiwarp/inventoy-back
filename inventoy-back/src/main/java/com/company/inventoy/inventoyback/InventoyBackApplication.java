2b9a7f051f1096a4e8dfab8414f1a186d9f5c75d
jhohan <jhohan@hotmail.com>
2023-08-30 05:40:21 -0500

    creacion app

---
+++ inventoy-back/src/main/java/com/company/inventoy/inventoyback/InventoyBackApplication.java
@@ -0,0 +1,13 @@
+package com.company.inventoy.inventoyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoyBackApplication.class, args);
+	}
+
+}
---
+++ inventoy-back/src/test/java/com/company/inventoy/inventoyback/InventoyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.inventoy.inventoyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
